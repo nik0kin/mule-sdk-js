15b3466ac82f1b6e999736ecbf27a6c90487e137
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2015-01-30 15:26:45 -0600

    set json dataType for all requests

--- src/utils/qwest.js
+++ src/utils/qwest.js
@@ -4,11 +4,11 @@ define(['../lib/qwest'], function (qwest) {
   var that = {};
 
   that.get = function (url, data, opts) {
-    return qwest.get(url, data, opts || {responseType:'json', withCredentials: true});
+    return qwest.get(url, data, opts || {dataType: 'json', responseType:'json', withCredentials: true});
   };
 
   that.post = function (url, data, opts) {
-    return qwest.post(url, data, opts || {responseType:'json', withCredentials: true});
+    return qwest.post(url, data, opts || {dataType: 'json', responseType:'json', withCredentials: true});
   };
 
   return that;
