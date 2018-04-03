288c02dc53ca9a3b9546b280224fc4ba651694c2
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2018-04-02 20:12:57 -0500

    expose all types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export { MockSdk } from './mockSdk/mock-sdk';
 export { sdk as MuleSdk } from './muleSdk/mule-sdk';
+
+export * from './types/mule';
+export * from './types/sdk';
+export * from './types/backend-sdk';
