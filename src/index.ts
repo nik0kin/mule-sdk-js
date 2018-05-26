a60d8d6c6e9482a3433fcc48dbd4c7e259b0c3e0
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2018-05-26 14:31:12 -0500

    fix unexposed http types + dist

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { MockSdk } from './mockSdk/mock-sdk';
 export { initializeMuleSdk } from './muleSdk/mule-sdk';
 
 export * from './types/mule';
+export * from './types/mule-http';
 export * from './types/sdk';
 export * from './types/backend-sdk';
